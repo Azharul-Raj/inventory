a8d137f85d98472a77fe6d65313a1bc057ab2f68
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-04-01 21:40:38 +0600

    working in user model

--- models/user.model.js
+++ models/user.model.js
@@ -10,16 +10,43 @@ const userSchema=new mongoose.Schema({
         required:[true,"Email is required"],
         validate:[validator.isEmail,"Please correct your email format."]
     },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+        validate:{
+            validator:(value)=>
+                validator.isStrongPassword(value,{
+                    minLength:6,
+                    minLowercase:1,
+                    minUppercase:1
+                }),            
+            message:"Password should contain min a lowercase a uppercase."
+        }
+    },
+    confirmPassword:{
+        type:String,
+        required:[true,"Type your confirm Password is required"],
+        validate:{
+            validator:(value)=>
+                validator.isStrongPassword(value,{
+                    minLength:6,
+                    minLowercase:1,
+                    minUppercase:1
+                }),            
+            message:"Password should contain min a lowercase a uppercase."
+        }
+    },
     contactNumber:String,
     role:{
         type:String,
         required:[true,"Role selection is required"],
         enum:{
-            values:["user","seller","admin","modaretor","manager"],
+            values:["user","seller","admin","modaretor","manager","supplier"],
             message:"{VALUE} is not a correct role.Please pass a right role."
         }
     },
-    location:String
+    location:String,
+    brand:String
 })
 
 module.exports.User=mongoose.model("User",userSchema);
